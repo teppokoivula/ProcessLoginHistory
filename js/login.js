2b42dec14592be255b92bc1817d6f70098c50210
TeppoKoivula <teppo@piikkilanka.com>
2012-11-07 01:05:00 +0200

    Remove duplicate window size field and add viewport meta tag

--- js/login.js
+++ js/login.js
@@ -9,11 +9,13 @@ $(function() {
           if (navigator.mimeTypes ["application/x-shockwave-flash"] != undefined) flash = 1;
       }
 
+      // add viewport meta tag
+      $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />');
+
       // login form extra fields
       $('form#ProcessLoginForm').append('<input type="hidden" name="javascript" value="1" />');
       $('form#ProcessLoginForm').append('<input type="hidden" name="flash" value="'+flash+'" />');
       $('form#ProcessLoginForm').append('<input type="hidden" name="screen" value="'+screen.width+'x'+screen.height+'" />');
-      $('form#ProcessLoginForm').append('<input type="hidden" name="window" value="'+window.innerWidth+'x'+window.innerHeight+'" />');
       $('form#ProcessLoginForm').append('<input type="hidden" name="window" value="'+window.innerWidth+'x'+window.innerHeight+'" />');
 
 });
