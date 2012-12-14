a2650ba9fde9cd6775164905e7df5f32ad95c4cc
TeppoKoivula <teppo@piikkilanka.com>
2012-12-14 19:39:23 +0200

    Make it possible to remove rows without opening them first

--- js/script.js
+++ js/script.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
     var less = $('span[data-term=less]').text();
     $('table.login-history tr').each(function() {
         $(this)
-            .find('td:eq(3)')
+            .find('td:eq(4)')
                 .addClass('user-agent')
                 .wrapInner('<span></span>')
                 .find('span')
                     .after('<a href="#" class="toggle-more">'+more+' <b>&or;</b></a>');
     });
     // more/less functionality
-    $('table.login-history th:eq(3)').css('width', '100px');
+    $('table.login-history th:eq(4)').css('width', '100px');
     $('table.login-history td.user-agent a').toggle(function() {
         $(this)
             .html(less + ' <b>&and;</b>')
             .parents('tr:first')
                 .addClass('open')
-                .after('<tr class="more '+$(this).prev('span').find('#device-type td').text()+'-device"><td colspan="4"><div>'+$(this).prev('span').html()+'</div></td></tr>');
+                .after('<tr class="more '+$(this).prev('span').find('#device-type td').text()+'-device"><td colspan="5"><div>'+$(this).prev('span').html()+'</div></td></tr>');
         return false;
     }, function() {
         $(this)
