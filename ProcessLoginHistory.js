19e5a4da2d2bc1f8e38a779629dc6a6cc8207a24
teppokoivula <teppo@piikkilanka.com>
2014-01-04 12:19:35 +0200

    Add option for logging IP addresses
    Storing IP address required minor change (addition of new ip_address
    field) to table used for storing data. Config option schema_version,
    constant SCHEMA_VERSION and method updateDatabaseSchema() were added
    specifically for this purpose (keeping database schema up to date.)

--- ProcessLoginHistory.js
+++ ProcessLoginHistory.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
     var less = $('span[data-term=less]').text();
     $('table.history tr').each(function() {
         $(this)
-            .find('td:eq(4)')
+            .find('td:eq(5)')
                 .addClass('user-agent')
                 .wrapInner('<span></span>')
                 .find('span')
                     .after('<a href="#" class="toggle-more">'+more+' <b>&or;</b></a>');
     });
     // more/less functionality
-    $('table.history th:eq(4)').css('width', '100px');
+    $('table.history th:eq(5)').css('width', '100px');
     $('table.history td.user-agent a').toggle(function() {
         $(this)
             .html(less + ' <b>&and;</b>')
             .parents('tr:first')
                 .addClass('open')
-                .after('<tr class="more '+$(this).prev('span').find('#device-type td').text()+'-device"><td colspan="5"><div>'+$(this).prev('span').html()+'</div></td></tr>');
+                .after('<tr class="more '+$(this).prev('span').find('#device-type td').text()+'-device"><td colspan="6"><div>'+$(this).prev('span').html()+'</div></td></tr>');
         return false;
     }, function() {
         $(this)
