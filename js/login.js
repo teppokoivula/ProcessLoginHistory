20c8b2af49f6182d95905b890364089e728df1cf
Teppo Koivula <teppo.koivula@gmail.com>
2023-05-06 22:16:02 +0300

    Remove Flash check

--- js/login.js
+++ js/login.js
@@ -1,16 +1,5 @@
 $(function() {
 
-    // check for flash support
-    var flash = 0;
-    try {
-        var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-        if (fo) flash = 1;
-    } catch(e) {
-        if (navigator.mimeTypes ["application/x-shockwave-flash"] != undefined) {
-            flash = 1;
-        }
-    }
-
     // add viewport meta tag?
     if (!document.querySelector('meta[name="viewport"]')) {
         $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />');
@@ -19,7 +8,6 @@ $(function() {
     // login form extra fields
     if (document.getElementById('ProcessLoginForm')) {
         $('form#ProcessLoginForm').append('<input type="hidden" name="javascript" value="1" />');
-        $('form#ProcessLoginForm').append('<input type="hidden" name="flash" value="'+flash+'" />');
         $('form#ProcessLoginForm').append('<input type="hidden" name="screen" value="'+screen.width+'x'+screen.height+'" />');
         $('form#ProcessLoginForm').append('<input type="hidden" name="window" value="'+window.innerWidth+'x'+window.innerHeight+'" />');
     }
